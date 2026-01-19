329ac998cc3d700b34d5fb04131643ca0418e635
Malcom Godlike <ivmno1special@gmail.com>
2026-01-19 14:30:14 -0500

    - Created PymordialDevice base class

---
+++ src/pymordial/core/pymordial_device.py
@@ -0,0 +1,73 @@
+from abc import ABC, abstractmethod
+from logging import DEBUG, basicConfig, getLogger
+
+
+class PymordialDevice(ABC):
+    logger = getLogger("PymordialDevice")
+    basicConfig(
+        level=DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+
+    @abstractmethod
+    def connect(self):
+        pass
+
+    @abstractmethod
+    def disconnect(self):
+        pass
+
+    @abstractmethod
+    def run_command(self):
+        pass
+
+    @abstractmethod
+    def open_app(self):
+        pass
+
+    @abstractmethod
+    def is_app_running(self):
+        pass
+
+    @abstractmethod
+    def show_recent_apps(self):
+        pass
+
+    @abstractmethod
+    def close_app(self):
+        pass
+
+    @abstractmethod
+    def tap(self):
+        pass
+
+    @abstractmethod
+    def type_text(self):
+        pass
+
+    @abstractmethod
+    def go_home(self):
+        pass
+
+    @abstractmethod
+    def press_enter(self):
+        pass
+
+    @abstractmethod
+    def press_esc(self):
+        pass
+
+    @abstractmethod
+    def capture_screenshot(self):
+        pass
+
+    @abstractmethod
+    def start_stream(self):
+        pass
+
+    @abstractmethod
+    def stop_stream(self):
+        pass
+
+    @abstractmethod
+    def get_latest_frame(self):
+        pass
